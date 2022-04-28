2b765ed4f58b1368a01a7a36425016a75de72e74
mdzam18 <mdzam18@freeuni.edu.ge>
2022-04-28 23:29:11 +0400

    add methods for emulator

--- Emulator/src/AssemblyEmulator.java
+++ Emulator/src/AssemblyEmulator.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 public class AssemblyEmulator {
 
-    private Map<String, Double> registers;
+    private Map<String, Integer> registers;
 
-    public AssemblyEmulator(String text){
+    public AssemblyEmulator(String text) {
         text = text.replaceAll(" ", ""); //Delete white spaces.
         text = text.toUpperCase(Locale.ROOT);
         registers = new HashMap<>();
@@ -14,41 +15,111 @@ public class AssemblyEmulator {
         printMap();
     }
 
-    private void printMap(){
-        for(String key : registers.keySet()){
+    private void printMap() {
+        for (String key : registers.keySet()) {
             System.out.println(key + " " + registers.get(key));
         }
     }
 
-    private String getInstructionType(String line){
+    private String getInstructionType(String line) {
         return "";
     }
 
-    private void fillRegistersMaps(String line){
+    private void fillRegistersMaps(String line) {
         int index = line.indexOf("=");
         String leftSide = getLeftSide(line.substring(0, index));
         String rightSide = getRightSide(line.substring(index + 1, line.indexOf(";")));
-        registers.put(leftSide, Double.parseDouble(rightSide));
+        registers.put(leftSide, Integer.parseInt(rightSide));
     }
 
-    private String getLeftSide(String str){
-        if(str.startsWith("R")){
+    private String getLeftSide(String str) {
+        if (str.startsWith("R")) {
             return str;
+        } else if (str.startsWith("M")) {
+            String address = str.substring(str.indexOf("[") + 1, str.indexOf("]"));
+            int res = getAddress(address);
         }
         return "";
     }
 
-    private String getRightSide(String str){
+    private int getAddress(String address) {
+        if (isNumber(address)) {
+            //amoige konkretuli misamartidan.
+        } else if (Character.isDigit(address.charAt(0))) {
+            //anu ragac aseti weria 2 + R1
+            char operator = getOperator(address);
+            int index = address.indexOf(operator);
+            int a = Integer.parseInt(address.substring(0, index));
+            int b = 0;
+            if (address.charAt(index + 1) == 'R') {
+                b = registers.get(address.substring(index + 1));
+            } else {
+                //anu sp-a.
+            }
+            return compute(a, b, operator);
+        } else if (address.startsWith("R")) {
+            //sheidzleba marto registri iyos an r + 4
+            if (containsOperator(address)) {
+                char operator = getOperator(address);
+                int index = address.indexOf(operator);
+                int a = registers.get(address.substring(0, index));
+                int b = Integer.parseInt(address.substring(index + 1));
+                return compute(a, b, operator);
+            } else {
+                //anu marto registria
+                return registers.get(address);
+            }
+        } else {
+            //sp-a.
+        }
+        return -1;
+    }
+
+    private boolean containsOperator(String address) {
+        return address.indexOf('+') != -1 || address.indexOf('-') != -1 || address.indexOf('*') != -1 || address.indexOf('/') != -1;
+    }
+
+    private int compute(int a, int b, char operator) {
+        if (operator == '+') {
+            return a + b;
+        } else if (operator == '-') {
+            return a - b;
+        } else if (operator == '*') {
+            return a * b;
+        } else {
+            return a / b;
+        }
+    }
+
+    private char getOperator(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '+' || str.charAt(i) == '-' || str.charAt(i) == '*' || str.charAt(i) == '/') {
+                return str.charAt(i);
+            }
+        }
+        return 0;
+    }
+
+    private boolean isNumber(String address) {
+        for (int i = 0; i < address.length(); i++) {
+            if (!Character.isDigit(address.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String getRightSide(String str) {
         //loads constants.
-        if(Character.isDigit(str.charAt(0))){
+        if (Character.isDigit(str.charAt(0))) {
             return str;
-        } else if(str.charAt(0) == '-' && Character.isDigit(str.charAt(1))){ //is negative number.
+        } else if (str.charAt(0) == '-' && Character.isDigit(str.charAt(1))) { //is negative number.
             return str;
         }
         return "";
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         AssemblyEmulator emulator = new AssemblyEmulator("R1 =- 5  ;");
     }
 }
