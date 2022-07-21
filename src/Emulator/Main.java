0942d49effaa4ce4224b1e462662f50ca71036ee
mdzam18 <56314167+mdzam18@users.noreply.github.com>
2022-07-21 14:09:00 +0400

    Update Main.java

--- src/Emulator/Main.java
+++ src/Emulator/Main.java
@@ -6,16 +6,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-          //String fileName = "C:\\Users\\mdzam\\Desktop\\assembly\\Assembly-Debugger\\src\\Tests\\calltests.txt";
-          String fileName = "D:\\FINAL\\Assembly-Debugger\\src\\Tests\\calltests.txt";
-//        String[] arr = new String[2];
-//        arr[0] = fileName;
-//        fileName = "\\Users\\mdzam\\Desktop\\assembly\\Assembly-Debugger\\Emulator\\src\\tests\\sum.txt";
-//        arr[1] = fileName;
-    //    fileName = createNewFile(arr);
-        // String fileName = args[0];
-=======
         //String fileName = "C:\\Users\\mdzam\\Desktop\\assembly\\Assembly-Debugger\\src\\Tests\\calltests.txt";
         String fileName = "src/Tests/calltests.txt";
         String fileName2 = "src/Tests/sum.txt";
@@ -24,7 +14,6 @@ public class Main {
         arr[1] = fileName2;
         FilesManager manager = new FilesManager();
         fileName = manager.createNewFile(arr);
->>>>>>> 48b66c88dacaefea3822811e91e38a995155d0dc
         AssemblyEmulator emulator = new AssemblyEmulator(new FileReader(fileName));
         emulator.next();
         emulator.next();
