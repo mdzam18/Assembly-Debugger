ae5ad8a46c85f95a9aa3292982cef3d999e533ff
nroga18 <nroga18@freeuni.edu.ge>
2022-08-01 11:27:54 +0400

    test connection between vs code and java project

--- src/Emulator/Main/Main.java
+++ src/Emulator/Main/Main.java
@@ -2,11 +2,55 @@ package src.Emulator.Main;
 
 import src.Emulator.AssemblyEmulator.AssemblyEmulator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        String text
+                = "usowushwbgsola ";
+        for (String arg: args
+             ) {
+
+            text += arg + " bla ";
+        }
+
+        // Try block to check if exception occurs
+        try {
+
+            // Create a FileWriter object
+            // to write in the file
+            FileWriter fWriter = new FileWriter(
+                    "D:\\FINAL\\Assembly-Debugger\\src\\Emulator\\Main\\testInputFile");
+
+            // Writing into file
+            // Note: The content taken above inside the
+            // string
+            fWriter.write(text);
+
+            // Printing the contents of a file
+            System.out.println(text);
+
+            // Closing the file writing connection
+            fWriter.close();
+
+            // Display message for successful execution of
+            // program on the console
+            System.out.println(
+                    "File is created successfully with the content.");
+        }
+
+        // Catch block to handle if exception occurs
+        catch (IOException e) {
+
+            // Print the exception
+            System.out.print(e.getMessage());
+        }
+
         String fileName = "src/Tests/sum.txt";
         String fileName2 = "src/Tests/calltests.txt";
         String arr[] = new String[2];
